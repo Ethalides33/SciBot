6b4c56022b656c39b36a1d3c138db63f5a05b97f
Ethalides33 <81507264+Ethalides33@users.noreply.github.com>
2022-02-16 23:24:48 +0100

    Create words.py
    Constant values

---
+++ words.py
@@ -0,0 +1,3 @@
+HELLO = ['hi','hello','bonjour','salut','hallo','holà','안녕하십니까','你好','dzień dobry','こんにちは','kon\'nichiwa','nǐ hǎo','annyeonghasibnikka','hei','merhaba','alo','أهلا','ahlan','שלום','ciao','สวัสดี','S̄wạs̄dī','Здравствуйте','zdravstvuyte','xin chào','ahoj','zdravo','Привет','privet','салам','salam','sveiki','tere','გამარჯობა','gamarjoba','tena koutou','sawubona','helo','halo','नमस्ते','namastē','nuqneH','olá','máriessë','aloha','γεια σας','geia sas','salve','përshëndetje']
+
+TY = ['You\'re welcome','Ask whenever','My pleasure','Hope this helped!']
